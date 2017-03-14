3732996492c9c61c28ec1d10b440e932b42e976d
Cody Coaster <thecodecoaster@gmail.com>
2017-03-14 02:47:47 -0400

    GetMessages function and replaced mongodb with mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var app = express();
 
+
 var bodyParser = require('body-parser');
 
-var mongo = require('mongodb').MongoClient;
+//var mongo = require('mongodb').MongoClient;
+var mongoose = require('mongoose');
 
 var database;
-
+var Message = mongoose.model('Message',{
+    msg: String,
+});
 
 app.use(bodyParser.json());
 
@@ -16,19 +20,33 @@ app.use(function(req, res, next){
     next();
 });
 
-mongo.connect('mongodb://localhost:27017/message', function(err,db){
+mongoose.connect('mongodb://localhost:27017/message', function(err,db){
     if(!err){
         console.log("we are connected to mongodb on port 27017");
-        database = db;
-       // db.collection('messages').insertOne({'msg':'test'});
+        GetMessages();
     }
 });
 
 app.post('/api/message', function(req, res){
     console.log(req.body);
-    database.collection('messages').insertOne(req.body);
+   
+   
+   //method with mongodb npm
+   // database.collection('messages').insertOne(req.body);
+
+     var message = new Message(req.body);
+       message.save();
     res.status(200);
 });
+
+
+function GetMessages(){
+    Message.find({}).exec(function(err, result){
+        console.log(result);
+    });
+}
+
+
 
 var server = app.listen(5000, function(){
     console.log('listening on port', server.address().port);
